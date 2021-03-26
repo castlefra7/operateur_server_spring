cc3b9372260b95a7cfde3a48846d7bd8efa4d238
Andrianina Rabakoson <andri.bakoson@gmail.com>
2021-03-26 09:25:57 +0300

    heroku deploy

--- src/main/java/mg/operateur/conn/ConnGen.java
+++ src/main/java/mg/operateur/conn/ConnGen.java
@@ -5,6 +5,8 @@
  */
 package mg.operateur.conn;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -19,13 +21,21 @@ public class ConnGen {
         Class.forName("org.postgresql.Driver");
     }
     
-    public static Connection getConn() throws SQLException {
-        String url = "jdbc:postgresql://localhost/db_operateur";
-        Properties props = new Properties();
-        props.setProperty("user","operateur");
-        props.setProperty("password","123456");
-        //props.setProperty("ssl","true");
-        Connection conn = DriverManager.getConnection(url, props);
+    public static Connection getConn() throws SQLException, URISyntaxException {
+        Connection conn = null;
+        if (System.getenv("DATABASE_URL") != null) {
+            
+            URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+            String username = dbUri.getUserInfo().split(":")[0];
+            String password = dbUri.getUserInfo().split(":")[1];
+            String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+
+            conn = DriverManager.getConnection(dbUrl, username, password);
+        } else {
+            String dbUrl = "jdbc:postgresql://" + "localhost" + ':' + "5432" + "/db_operateur";
+            conn = DriverManager.getConnection(dbUrl, "operateur", "123456");
+        }
         return conn;
     }
 }
